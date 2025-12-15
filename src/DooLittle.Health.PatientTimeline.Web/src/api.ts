0a8e5f1ac71f60080bf9819599178b8c4274a019
Nithin Mohan T K <nith>
2025-12-15 18:23:06 +0000

    Fixing the API data

--- src/DooLittle.Health.PatientTimeline.Web/src/api.ts
+++ src/DooLittle.Health.PatientTimeline.Web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Patient, FHIRTimelineData } from './types';
 
-const API_BASE_URL = 'http://localhost:5002/api';
+const API_BASE_URL = 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
